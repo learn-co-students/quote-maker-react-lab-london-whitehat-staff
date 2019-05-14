67830d7661321010f373c7d49b7ca2535635e084
Anna Burley <anna.burley@sky.uk>
2019-05-14 15:10:11 +0100

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+
+export default class QuoteCard extends Component {
+  // handleClick = () =>{
+  //   this.props.upvote(this.props.quote.id);
+  // };
+
+  handleClick = (event) => {
+    console.log('ssss', event.target.dataset);
+    if (event.target.dataset.action === 'upvote') {
+      this.props.upvote(this.props.quote.id);
+    }
+
+    if (event.target.dataset.action === 'downvote') {
+      this.props.downvote(this.props.quote.id);
+    }
+    if(event.target.dataset.action === 'remove') {
+      this.props.remove(this.props.quote.id);
+    }
+  };
+
+  render() {
+    const props = this.props;
+    return(
+      <div>
+        <div className="card card-inverse card-success card-primary mb-3 text-center">
+          <div className="card-block">
+            <blockquote className="card-blockquote">
+              <p>{props.quote.content}</p>
+              <footer>- author <cite title="Source Title">{props.quote.author}</cite></footer>
+            </blockquote>
+          </div>
+          <div className="float-right">
+            <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-action="upvote"
+                onClick={this.handleClick}
+              >
+                Upvote
+              </button>
+              <button
+                type="button"
+                data-action="downvote"
+                className="btn btn-secondary"
+                onClick={this.handleClick}
+              >
+                Downvote
+              </button>
+              <button
+                type="button"
+                data-action="remove"
+                className="btn btn-danger"
+                onClick={this.handleClick}
+              >
+                &times; REMOVE
+              </button>
+            </div>
+            {/* <div>Votes: {Render Quote Votes}</div> */}
+            <div>Votes: {props.quote.votes}</div>
+          </div>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
       </div>
-    </div>
-  </div>;
-
-export default QuoteCard;
+    )
+  }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,38 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case 'ADD_QUOTE':
+      return [...state, {...action.quote, votes: 0}];
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      let idx;
+      console.log('action', action.quoteId);
+
+      return state.map(arrayElement => {
+        console.log('arrayElement 0->> ', arrayElement);
+
+        // check if ID of elemet is same as action.quoteId and return an updared object
+        if(arrayElement.id === action.quoteId){
+          arrayElement.votes = arrayElement.votes + 1;
+        }
+        return arrayElement;
+      });
+
+    case 'DOWNVOTE_QUOTE':
+      return state.map(arrayElement => {
+        if(arrayElement.id === action.quoteId){
+          if(arrayElement.votes === 0){
+            return arrayElement;
+          }
+          arrayElement.votes = arrayElement.votes - 1;
+          return arrayElement;
+        }
+      });
+
+    default:
+      return state;
+  }
 }
+
